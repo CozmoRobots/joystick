6aa885033b213cd3d6121049426ecbdf52de72d5
mudathirmahgoub <mudathirmahgoub@gmail.com>
2019-02-13 06:34:46 -0600

    Diagonals

--- xbox_controller.py
+++ xbox_controller.py
@@ -3,6 +3,20 @@ from xinput import XInputJoystick
 import cozmo
 from cozmo.util import *
 
+directional_pad_speeds = {
+    # up, down, left, right
+    GAMEPAD_DPAD_UP: (100, 100),
+    GAMEPAD_DPAD_DOWN: (-100, -100),
+    GAMEPAD_DPAD_LEFT: (0, 100),
+    GAMEPAD_DPAD_RIGHT: (100, 0),
+    # diagonals
+    GAMEPAD_DPAD_UP | GAMEPAD_DPAD_LEFT: (50, 100),
+    GAMEPAD_DPAD_UP | GAMEPAD_DPAD_RIGHT: (100, 50),
+    GAMEPAD_DPAD_DOWN | GAMEPAD_DPAD_LEFT: (-50, -100),
+    GAMEPAD_DPAD_DOWN | GAMEPAD_DPAD_RIGHT: (-100, -50),
+}
+
+
 def cozmo_program(robot: cozmo.robot.Robot):
 
     joysticks = XInputJoystick.enumerate_devices()
@@ -16,24 +30,15 @@ def cozmo_program(robot: cozmo.robot.Robot):
     joystick = joysticks[0]
 
     def on_state_changed(state):
-
         state = struct_dict(state.gamepad)
-
-        def switch_directional_pad(button):
-            (left_speed, right_speed) = {
-                GAMEPAD_DPAD_UP: (100, 100),
-                GAMEPAD_DPAD_DOWN: (-100, -100),
-                GAMEPAD_DPAD_LEFT: (0, 100),
-                GAMEPAD_DPAD_RIGHT: (100, 0),
-            }.get(button, (0, 0))    # 9 is default if x not found
-            robot.drive_wheels(left_speed, right_speed)
-
-        switch_directional_pad(state['buttons'])
+        (left_speed, right_speed) = directional_pad_speeds.get(state['buttons'], (0, 0))
+        robot.drive_wheels(left_speed, right_speed)
 
     joystick.on_state_changed = on_state_changed
 
     while True:
         joystick.dispatch_events()
         time.sleep(.01)
+
 
 cozmo.run_program(cozmo_program)
